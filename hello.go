837f7285b57a82c20187a39d3d1560fec75062b4
Kadir Elmacı <abdulkadirelmaci@gmail.com>
2022-06-19 11:41:33 +0300

    Example create and read

--- hello.go
+++ hello.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Human struct {
+	Name string
+	Age  int16
+}
+
+var humans []Human
 
 func main() {
-    fmt.Println("Hello, World!")
+
+	http.HandleFunc("/get", get)
+	http.HandleFunc("/post", post)
+	http.ListenAndServe(":8090", nil)
+}
+
+func get(w http.ResponseWriter, req *http.Request) {
+
+	encoder := json.NewEncoder(w)
+	encoder.Encode(humans)
+}
+
+func post(w http.ResponseWriter, req *http.Request) {
+
+	if req.Method == http.MethodPost {
+		var h Human
+		json.NewDecoder(req.Body).Decode(&h)
+		humans = append(humans, h)
+		fmt.Fprintf(w, "Appended\n")
+	} else {
+		fmt.Fprintln(w, "Not supported")
+	}
+
 }
